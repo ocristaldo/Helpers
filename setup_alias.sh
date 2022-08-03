61adb91be458bd5d645129c89a5040a36592081b
ocristaldo <51077821+ocristaldo@users.noreply.github.com>
2022-08-03 20:29:15 +1000

    Create setup_alias.sh

---
+++ setup_alias.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+aliases=$(cat <<EOF
+
+## Colorize the ls output ##
+alias ls='ls -CF --color=auto'
+ 
+## Use a long listing format ##
+alias ll='ls -la'
+
+alias lf='ls -alF'
+
+alias lt='ls --human-readable --size -1 -S --classify'
+
+alias lu='du -sh * | sort -h'  
+ 
+## Show hidden files ##
+alias l.='ls -d .* --color=auto'
+
+## get rid of command not found ##
+alias cd..='cd ..'
+
+## Update
+alias sup="sudo apt update && sudo apt upgrade -y"
+alias purge="sudo apt autoremove && apt purge"
+
+## Colorize the grep command output for ease of use (good for log files)##
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+# install  colordiff package :)
+alias diff='colordiff'
+
+alias mount='mount |column -t'
+
+# handy short cuts #
+alias h='history'
+alias j='jobs -l'
+
+## Path and date
+alias path='echo -e ${PATH//:/\\n}'
+alias now='date +"%T"'
+alias nowtime=now
+alias nowdate='date +"%d-%m-%Y"'
+
+## Use vim
+alias vi=vim
+alias svi='sudo vi'
+alias vis='vim "+set si"'
+alias edit='vim'
+
+## Services
+alias whatsup='service --status-all'  
+
+## GIT
+alias gs='git status'
+alias gb='git branch'  
+alias gr='git remote -v'
+
+EOF
+)
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied, please choose one of them : bash or zsh"
+elif [ "$1" == "bash" ]; then
+    shift
+    echo "${aliases}" >> $HOME/.bashrc
+    source $HOME/.bashrc
+    echo "Done, open a new terminal"
+elif [ "$1" == "zsh" ]; then
+    shift
+    echo "${aliases}" >> $HOME/.zshrc
+    source $HOME/.zshrc
+    echo "Done, open a new terminal"
+fi
